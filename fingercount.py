26a789bf4952828cf8453a3721d15759c74578e5
BanerjeeRaunak99 <43110997+BanerjeeRaunak99@users.noreply.github.com>
2021-05-04 11:01:44 +0530

    fixed false positive problem

--- fingercount.py
+++ fingercount.py
@@ -1,6 +1,7 @@
 
 import cv2
 import numpy as np
+import math
 cap = cv2.VideoCapture(0)
 c = 0
 upper_left = (0, 0)
@@ -14,8 +15,8 @@ while True:
     rect_img = image[upper_left[1] : bottom_right[1], upper_left[0] : bottom_right[0]]
     sketcher_rect = rect_img
     hsvim = cv2.cvtColor(sketcher_rect, cv2.COLOR_BGR2HSV)
-    lower = np.array([0, 10, 60], dtype = "uint8")
-    upper = np.array([20, 150, 255], dtype = "uint8")
+    lower = np.array([0,20,70], dtype = "uint8")
+    upper = np.array([20,255,255], dtype = "uint8")
     skinRegionHSV = cv2.inRange(hsvim, lower, upper)
     blurred = cv2.blur(skinRegionHSV, (2,2))
     ret,thresh = cv2.threshold(blurred,0,255,cv2.THRESH_BINARY)
@@ -23,14 +24,17 @@ while True:
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = max(contours, key=lambda x: cv2.contourArea(x))
     cv2.drawContours(image, [contours], -1, (255,255,0), 2)
+    
     # cv2.imshow("contours", image)
     hull = cv2.convexHull(contours)
+    
+
     cv2.drawContours(image, [hull], -1, (0, 255, 255), 2)
     # cv2.imshow("hull", image)  
     hull = cv2.convexHull(contours, returnPoints=False)
     defects = cv2.convexityDefects(contours, hull)
-    if defects is not None:
-      cnt = 0
+    
+    cnt = 0
     for i in range(defects.shape[0]):  # calculate the angle
         s, e, f, d = defects[i][0]
         start = tuple(contours[s][0])
@@ -39,12 +43,15 @@ while True:
         a = np.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
         b = np.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
         c = np.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-        angle = np.arccos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  #      cosine theorem
-        if angle <= np.pi / 2:  # angle less than 90 degree, treat as fingers
+        angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57 #      cosine theorem
+        s = (a+b+c)/2
+        ar = math.sqrt(s*(s-a)*(s-b)*(s-c))
+        d=(2*ar)/a
+        if angle <= 90 and d>30:  # angle less than 90 degree, treat as fingers
             cnt += 1
             cv2.circle(image, far, 4, [0, 0, 255], -1)
-        if cnt > 0:
-            cnt = cnt+1
+        
+    cnt+=1
     cv2.putText(image, str(cnt), (0, 50), cv2.FONT_HERSHEY_SIMPLEX,1, (255, 0, 0) , 2, cv2.LINE_AA)
     cv2.imshow('final_result',image)
     # Show the image
